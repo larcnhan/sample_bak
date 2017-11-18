a23157cdefc52fd706bea6803805896df09d2973
LarcnHan <larcnhan@126.com>
2017-11-18 17:09:50 +0000

    update logout method

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -51,7 +51,7 @@ class SessionsController extends Controller
      * 注销
      * @return
      */
-    public function destory()
+    public function destroy()
     {
         Auth::logout();
         session()->flash('success', '您已成功退出！');
